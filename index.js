7d6724698552918c7932e4e2a090df0086d678b1
Brandon Pereira <brandonp@criticalmass.com>
2017-08-09 21:44:04 -0600

    work to user logins

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const db = require('./lib/database');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+require('dotenv').config();
 
-const list = new db.List({title: 'lol'});
-list.save((err, list) => {
-  console.log(err, list);
-})
-db.List.findOne({title: 'lol'})
-  .then((res) => console.log("from db finding: ",res))
-  .catch((err) => console.log(err));
+// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:8080/auth/google/callback"
+}, (accessToken, refreshToken, profile, cb) => {
+  db.User.findOrCreate(profile.id, {
+    google_id: profile.id,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName
+  }, (err, user) => cb(err, user))
+}));
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
 });
 
-require('./lib/routes/lists.js')(app);
+passport.deserializeUser(function(id, done) {
+  db.User.findOne({_id: id}, (err, obj) => done(err, obj.toJSON()));
+});
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.json(Object.assign({hello: 'world'}, req.user));
+});
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(8080);
+
+// require('./lib/routes/lists.js')(app);
