339212723e3fea5efcdc8d16cdb37045e8184bdb
Brandon Pereira <brandonp@criticalmass.com>
2018-09-17 16:36:35 -0600

    format file

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -6,35 +6,35 @@
  * @param {Error} err error stack
  */
 function handleUncaughtError(taskName, res, err) {
-  // ValidationError comes from Mongoose
-  if (err.name === "ValidationError") {
-    res.status(500).json({
-      error: `Error while ${taskName}`,
-      details: err.message
-    });
-  } else if (err.code === "AccessError") {
-    res.status(404).json({
-      error: err.message
-    });
-  } else if (err.code === "PermissionError") {
-    res.status(501).json({
-      error: err.message
-    });
-  } else {
-    console.error(`UnhandledError while ${taskName}`, err);
-    res.status(500).json({
-      error: `Unexpected error while ${taskName}`
-    });
-  }
+    // ValidationError comes from Mongoose
+    if (err.name === 'ValidationError') {
+        res.status(500).json({
+            error: `Error while ${taskName}`,
+            details: err.message
+        });
+    } else if (err.code === 'AccessError') {
+        res.status(404).json({
+            error: err.message
+        });
+    } else if (err.code === 'PermissionError') {
+        res.status(501).json({
+            error: err.message
+        });
+    } else {
+        console.error(`UnhandledError while ${taskName}`, err);
+        res.status(500).json({
+            error: `Unexpected error while ${taskName}`
+        });
+    }
 }
 
-function throwError(msg, code = "AccessError") {
-  const error = new Error(msg);
-  error.code = code;
-  throw error;
+function throwError(msg, code = 'AccessError') {
+    const error = new Error(msg);
+    error.code = code;
+    throw error;
 }
 
 module.exports = {
-  handleUncaughtError,
-  throwError
+    handleUncaughtError,
+    throwError
 };
