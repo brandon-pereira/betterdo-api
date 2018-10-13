b54c5c2ba72b31523f5cd44b9f7674f3faead5be
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:33:58 -0600

    tweak route

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,8 +3,8 @@ const express = require('express');
 module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.user) {
-            console.log('redirect to,', process.env.BASE_FOLDER + '/app',  'from root');
-            res.redirect(process.env.BASE_FOLDER + '/app');
+            console.log('redirect to', process.env.BASE_FOLDER + '/app',  'from root');
+            res.redirect('./app');
         } else {
             next();
         }
