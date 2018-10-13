801051d1904426e42eb7087d550ebd1f5c9519be
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:30:10 -0600

    added debug log

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,7 +3,7 @@ const express = require('express');
 module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.user) {
-            console.log('redirect to /app from root');
+            console.log('redirect to,', process.env.BASE_FOLDER + '/app',  'from root');
             res.redirect(process.env.BASE_FOLDER + '/app');
         } else {
             next();
