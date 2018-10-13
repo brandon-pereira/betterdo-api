d1da97483e7760cb7ed1fc28ff3425d7e115e700
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:27:59 -0600

    redirect

--- src/routes/root.js
+++ src/routes/root.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.user) {
             console.log('redirect to /app from root');
-            res.redirect('/app');
+            res.redirect(process.env.BASE_FOLDER + '/app');
         } else {
             next();
         }
