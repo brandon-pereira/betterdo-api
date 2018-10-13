25ce1bffc34b8b661f4185b79bf8a55f83bf8357
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:27:10 -0600

    tweaks to redirects

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,6 +3,7 @@ const express = require('express');
 module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.user) {
+            console.log('redirect to /app from root');
             res.redirect('/app');
         } else {
             next();
