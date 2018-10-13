bde35cdc38664632175a66874f5a8090c9968dda
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 13:52:38 -0600

    work to redirects

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,9 +3,8 @@ const express = require('express');
 module.exports = app => {
     /* Initialize a router, anything behind `/api` requires authentication. */
     app.use('/', (req, res, next) => {
-        console.log("HERE", req.baseUrl, req.url, req.mount);
         if (req.user) {
-            res.redirect('app');
+            res.redirect(req.baseUrl + '/app');
         } else {
             next();
         }
