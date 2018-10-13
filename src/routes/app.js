1407169525b574fccb0ed4be6612c20a04120938
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:11:20 -0600

    logging

--- src/routes/app.js
+++ src/routes/app.js
@@ -4,6 +4,7 @@ module.exports = app => {
     /* Initialize a router, anything behind `/api` requires authentication. */
     const _app = express.Router();
     _app.use('/', (req, res, next) => {
+        console.log("app", req.user);
         if (!req.user) {
             res.redirect(process.env.BASE_FOLDER);
         } else {
--- src/routes/root.js
+++ src/routes/root.js
@@ -3,8 +3,9 @@ const express = require('express');
 module.exports = app => {
     /* Initialize a router, anything behind `/api` requires authentication. */
     app.use('/', (req, res, next) => {
+        console.log("root", req.user);
         if (req.user) {
-            res.redirect(process.env.BASE_FOLDER + '/appsss');
+            res.redirect(process.env.BASE_FOLDER + '/app');
         } else {
             next();
         }
