50a0ddd624cb4521628f4f777e5fba4809869601
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 14:42:32 -0600

    tweak

--- src/routes/root.js
+++ src/routes/root.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.user) {
             console.log('redirect to', process.env.BASE_FOLDER + '/app',  'from root');
-            res.redirect('betterdo/app');
+            res.redirect('/betterdo/apps');
         } else {
             next();
         }
