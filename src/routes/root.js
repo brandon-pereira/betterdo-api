f8a57ae952513a89fdf2a336b0b5230d8e6f4438
Brandon Pereira <brandonp@criticalmass.com>
2018-10-13 13:43:39 -0600

    testing route change

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,6 +3,7 @@ const express = require('express');
 module.exports = app => {
     /* Initialize a router, anything behind `/api` requires authentication. */
     app.use('/', (req, res, next) => {
+        console.log("HERE", req.baseUrl, req.url, req.mount);
         if (req.user) {
             res.redirect('app');
         } else {
