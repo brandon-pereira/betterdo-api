c57cafa3165de22c4ead50195247b38809891958
Brandon Pereira <brandonp@criticalmass.com>
2018-11-26 18:44:31 -0700

    work to custom lists

--- src/controllers/init.js
+++ src/controllers/init.js
@@ -1,6 +1,9 @@
 async function init(listId = 'inbox', { database, user }) {
     let currentList = database.Lists.getLists(user._id, listId);
     let lists = database.Lists.getLists(user._id);
+    // console.log(lists);
+    // lists = lists.toArray();
+    // lists = lists.push(await database.Lists.getLists(user._id, listId));
     [currentList, lists] = await Promise.all([currentList, lists]);
     if (currentList === null) {
         // user passed in invalid list
