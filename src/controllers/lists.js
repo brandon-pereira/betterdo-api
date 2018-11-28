dcd27689df232a61e00120e827208f8cf5b873df
Brandon Pereira <brandonp@criticalmass.com>
2018-11-28 07:13:23 -0700

    api call to fetch completed tasks

--- src/controllers/lists.js
+++ src/controllers/lists.js
@@ -2,7 +2,7 @@ const { throwError } = require('../helpers/errorHandler');
 const { fetchHighPriority, fetchToday, fetchTomorrow } = require('../helpers/customLists');
 const countTasks = require('../helpers/countTasks');
 
-async function getLists(listId, { database, user }) {
+async function getLists(listId, { database, user, includeCompleted }) {
     // Get lists based on query data
     let lists = [];
     if (listId === 'high-priority') {
@@ -25,10 +25,12 @@ async function getLists(listId, { database, user }) {
     }
     // return appropriate results
     if (listId && lists) {
-        // counted completed tasks
-        const { incompleteTasks, completeTasks } = countTasks(lists.tasks);
-        lists.tasks = incompleteTasks;
-        lists.completedTasks = completeTasks.length;
+        if (!includeCompleted) {
+            // counted completed tasks
+            const { incompleteTasks, completeTasks } = countTasks(lists.tasks);
+            lists.tasks = incompleteTasks;
+            lists.completedTasks = completeTasks.length;
+        }
         return lists;
     } else if (listId) {
         // specific list but no results
@@ -64,17 +66,18 @@ async function updateList(listId, updatedList = {}, { database, user }) {
     if (list.type === 'inbox') {
         updatedList = { tasks: updatedList.tasks };
     }
-    if (
-        updatedList.tasks &&
-        (!Array.isArray(updatedList.tasks) ||
-            updatedList.tasks.length !== list.tasks.length ||
-            updatedList.tasks.find(
-                _id => !list.tasks.map(task => task._id.toString()).includes(_id)
-            ))
-    ) {
-        throwError('Invalid modification of tasks');
-        delete updatedList.tasks;
-    }
+    // const isShowingCompleted = true;
+    // const invalidIds = updatedList.tasks.find(
+    //     _id => !list.tasks.map(task => task._id.toString()).includes(_id)
+    // );
+    // if (
+    //     updatedList.tasks &&
+    //     (!Array.isArray(updatedList.tasks) ||
+    //     (updatedList.tasks.length !== list.tasks.length || ) ||
+    //     invalidIds
+    // ) {
+    throwError('Invalid modification of tasks');
+    // }
     // Merge the lists.. validation on the model will handle errors
     Object.assign(list, updatedList);
     // Save the model
