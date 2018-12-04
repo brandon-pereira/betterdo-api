41fc82c40aaf9ccd276a28666b595864fc4d4f3f
Brandon Pereira <brandonp@criticalmass.com>
2018-12-03 20:01:51 -0700

    tests

--- src/express.js
+++ src/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 app.set('strict routing', true);
+
 /**
  * Express middleware
  */
+app.use(require('connect-slashes'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').json({}));
 app.use(require('body-parser').urlencoded({ extended: true }));
