7b7ede48b441433b0b6e9cf89d5617ffad474718
Brandon Pereira <brandonp@criticalmass.com>
2018-12-03 20:34:22 -0700

    update app to use url resolver

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
+const url = require('url');
 
 module.exports = app => {
-    /* Initialize a router, anything behind `/app` requires authentication. */
+    // Force /app to redirect to /app/
     app.all('/app', (req, res) => {
         console.log('Redirect /app to /app/');
-        res.redirect(path.join(process.env.SERVER_URL, 'app'));
+        res.redirect(url.resolve(process.env.SERVER_URL, 'app'));
     });
 
+    // Anything behind `/app/` requires authentication.
     app.use('/app/', (req, res, next) => {
         console.log('HERE');
         if (req.user) {
