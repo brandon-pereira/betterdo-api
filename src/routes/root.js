9522cf5dbe234756aa3615c5b31bb64f0c80664c
Brandon Pereira <brandonp@criticalmass.com>
2018-12-03 20:32:39 -0700

    updated to use url method

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require('path');
-console.log(path.join(process.env.SERVER_URL, 'app'));
+const url = require('url');
+
 module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.path === '/' && req.user) {
             console.log('Redirect to app from root');
-            res.redirect(`${process.env.SERVER_URL}'app`);
+            res.redirect(url.resolve(process.env.SERVER_URL, 'app'));
         } else {
             next();
         }
