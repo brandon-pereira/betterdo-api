27535c780a613de3171a47a2d00798bbd0a18d5a
Brandon Pereira <brandonp@criticalmass.com>
2018-12-03 20:15:14 -0700

    testing change

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+console.log(path.join(process.env.SERVER_URL, 'app'));
 module.exports = app => {
     app.use('/', (req, res, next) => {
         if (req.path === '/' && req.user) {
             console.log('Redirect to app from root');
-            res.redirect(path.join(process.env.SERVER_URL, 'app'));
+            res.redirect(`${process.env.SERVER_URL}'app`);
         } else {
             next();
         }
