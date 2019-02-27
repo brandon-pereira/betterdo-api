c65bbcfc2ab4e82b5359b8060ab20149cb6c0bcf
Brandon Pereira <brandonp@criticalmass.com>
2019-02-26 23:08:35 -0700

    use mongodb adapter

--- src/notifier.js
+++ src/notifier.js
@@ -1,38 +1,21 @@
-const WebPushNotifications = require('web-pushnotifications');
+const { WebPushNotifications, MongoAdapter } = require('web-pushnotifications');
 
 module.exports = (app, db) => {
-    let notificationQueue = [];
     const vapidKeys = {
         publicKey: process.env.VAPID_PUBLIC_KEY,
         privateKey: process.env.VAPID_PRIVATE_KEY,
         email: 'brandonpereiras@gmail.com'
     };
+
     const getUserPushSubscription = async userId => {
         const user = await db.Users.findById(userId);
         return user.pushSubscriptions[0];
-    };
-    const scheduleNotification = (date, userId, payload) => {
-        const id = Math.random();
-        notificationQueue.push({
-            id,
-            date,
-            userId,
-            payload
-        });
-    };
-    const fetchNotifications = date => {
-        return notificationQueue.filter(notification => notification.date <= date);
-    };
-    const clearNotification = payload => {
-        notificationQueue = notificationQueue.filter(noti => noti.id !== payload.id);
     };
 
     const notifier = new WebPushNotifications({
         vapidKeys,
         getUserPushSubscription,
-        fetchNotifications,
-        scheduleNotification,
-        clearNotification
+        adapter: new MongoAdapter(db.connection)
     });
 
     // notifier.schedule(new Date(), 'a', {
