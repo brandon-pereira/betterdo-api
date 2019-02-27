e7cd99d47756ddcad47fa1e98f1c349bd91ff108
Brandon Pereira <brandonp@criticalmass.com>
2019-02-26 23:12:24 -0700

    refactor client

--- src/notifier.js
+++ src/notifier.js
@@ -1,19 +1,17 @@
 const { WebPushNotifications, MongoAdapter } = require('web-pushnotifications');
 
 module.exports = (app, db) => {
-    const vapidKeys = {
-        publicKey: process.env.VAPID_PUBLIC_KEY,
-        privateKey: process.env.VAPID_PRIVATE_KEY,
-        email: process.env.VAPID_EMAIL
-    };
-
     const getUserPushSubscription = async userId => {
         const user = await db.Users.findById(userId);
-        return user.pushSubscriptions[0];
+        return user.pushSubscriptions[user.pushSubscriptions.length - 1];
     };
 
     const notifier = new WebPushNotifications({
-        vapidKeys,
+        vapidKeys: {
+            publicKey: process.env.VAPID_PUBLIC_KEY,
+            privateKey: process.env.VAPID_PRIVATE_KEY,
+            email: process.env.VAPID_EMAIL
+        },
         getUserPushSubscription,
         adapter: new MongoAdapter(db.connection)
     });
