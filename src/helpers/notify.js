70b4ee4b2ab9c66cf210ebddbc2ecafa6abb0d68
Brandon Pereira <brandonp@criticalmass.com>
2019-03-31 09:55:39 -0600

    actually send the msg

--- src/helpers/notify.js
+++ src/helpers/notify.js
@@ -1,26 +1,21 @@
-module.exports.notifyAboutSharedList = (title, { notifier, list }) => {
+module.exports.notifyAboutSharedList = (title, { notifier, user, list }) => {
     const members = list.members;
     const listId = list._id;
     const isSharedList = members.length > 1;
 
     if (isSharedList) {
         members.forEach(async member => {
-            // if (member._id.toString() !== user._id.toString()) {
-            const d = new Date();
-            d.setMinutes(d.getMinutes() + 1);
-            const id = await notifier.schedule(d, member._id, {
-                title,
-                url: `${process.env.SERVER_URL}/app/${listId}`,
-                tag: `shared-list:${listId}`,
-                data: {
-                    listId: listId,
-                    listTitle: list.title
-                }
-            });
-            setTimeout(() => {
-                console.log('HERE');
-                notifier.cancelNotification(id);
-            }, 5000);
+            if (member._id.toString() !== user._id.toString()) {
+                await notifier.send(member._id, {
+                    title,
+                    url: `${process.env.SERVER_URL}/app/${listId}`,
+                    tag: `shared-list:${listId}`,
+                    data: {
+                        listId: listId,
+                        listTitle: list.title
+                    }
+                });
+            }
         });
     }
 };
