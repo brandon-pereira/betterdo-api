aa5007d292df4acab63facd87265251589eafc96
Brandon Pereira <brandonp@criticalmass.com>
2021-10-04 22:51:10 -0600

    use memory db

--- test/globalSetup.ts
+++
@@ -1,7 +0,0 @@
-process.env.DATABASE_NAME = 'betterdo-unitTests';
-
-export default async (): Promise<void> => {
-    // Ideally we can connect here but dynamic import causes massive babel issues, ignoring it for now.
-    // const { connect } = await import('../src/database');
-    // await connect();
-};
--- test/globalTeardown.ts
+++ test/globalTeardown.ts
@@ -1,11 +1,5 @@
-import database, { connect, disconnect } from '../src/database';
+import { disconnect } from '../src/database';
 
 export default async (): Promise<void> => {
-    await connect();
-    // Sleep for a second, fixes raise conditions :shrug:
-    await sleep();
-    await database.connection.db.dropDatabase();
     await disconnect();
 };
-
-const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
