1c83d06feba3358342590d5de58274d6730772a4
Brandon Pereira <brandonpereiras@gmail.com>
2021-11-22 21:19:04 -0700

    fix globalsettup issue

--- test/global-setup.ts
+++ test/global-setup.ts
@@ -1,3 +1,7 @@
-export default (): void => {
+// @ts-expect-error: not bothering adding typing for setup file for this package
+import jestMongoSetup from '@shelf/jest-mongodb/setup';
+
+export default async (): Promise<void> => {
     process.env.TZ = 'America/Edmonton';
+    await jestMongoSetup();
 };
