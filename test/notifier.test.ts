ea30bbb34c503f4947824270675724cc86c9b1b1
Brandon Pereira <brandonpereiras@gmail.com>
2021-11-26 17:21:15 -0700

    tests for notifications

--- test/notifier.test.ts
+++ test/notifier.test.ts
@@ -1,7 +1,11 @@
-/* eslint-disable jest/no-commented-out-tests */
+import './helpers/toMatchObject';
 import createRouter from './helpers/createRouter';
 import { updateUser } from '../src/controllers/users';
 import { connect, disconnect } from '../src/database';
+import { createList, updateList } from '../src/controllers/lists';
+import { RouterOptions } from '../src/helpers/routeHandler';
+import { List } from '../src/schemas/lists';
+import { createTask, deleteTask, updateTask } from '../src/controllers/tasks';
 
 beforeAll(async () => {
     await connect();
@@ -39,11 +43,66 @@ describe('Notifier', () => {
         });
     });
 
-    // describe('Shared List', () => {
-    //     test('adds task', async () => {});
-    //     test('marks task completed', async () => {});
-    //     test('updates task', async () => {});
-    //     test('deletes task', async () => {});
-    //     test('supports incremental updates?', async () => {});
-    // });
+    describe('Shared List', () => {
+        let sharedList: List | null = null;
+        let user1: RouterOptions | null = null;
+        let user2: RouterOptions | null = null;
+
+        beforeAll(async () => {
+            user1 = await createRouter();
+            user2 = await createRouter();
+            sharedList = await createList({ title: 'shared list' }, user1);
+            sharedList = await updateList(
+                sharedList._id,
+                { members: [user1.user._id, user2.user._id] },
+                user1
+            );
+        });
+
+        test('adds task', async () => {
+            if (!user1 || !user2 || !sharedList) return;
+            expect(user1?.notifier.send).toBeCalledTimes(0);
+            await createTask(sharedList._id, { title: 'just do it' }, user1);
+            expect(user1?.notifier.send).toBeCalledTimes(1);
+            const payload = (user1?.notifier.send as jest.Mock).mock.calls[0];
+            expect(payload[0]).toMatchObject(user2?.user._id);
+            expect(payload[1].title).toBe('unitTest added just do it to shared list.');
+        });
+
+        test('marks task completed', async () => {
+            if (!user1 || !user2 || !sharedList) return;
+            expect(user1?.notifier.send).toBeCalledTimes(0);
+            const task = await createTask(sharedList._id, { title: 'just do it' }, user1);
+            // reset notification from creation
+            (user1?.notifier.send as jest.Mock).mockReset();
+            await updateTask(task._id, { isCompleted: true }, user1);
+            const payload = (user1?.notifier.send as jest.Mock).mock.calls[0];
+            expect(payload[0]).toMatchObject(user2?.user._id);
+            expect(payload[1].title).toBe('unitTest completed just do it in shared list.');
+        });
+
+        test('updates task', async () => {
+            if (!user1 || !user2 || !sharedList) return;
+            expect(user1?.notifier.send).toBeCalledTimes(0);
+            const task = await createTask(sharedList._id, { title: 'old' }, user1);
+            // reset notification from creation
+            (user1?.notifier.send as jest.Mock).mockReset();
+            await updateTask(task._id, { title: 'new' }, user1);
+            const payload = (user1?.notifier.send as jest.Mock).mock.calls[0];
+            expect(payload[0]).toMatchObject(user2?.user._id);
+            expect(payload[1].title).toBe('unitTest updated new in shared list.');
+        });
+
+        test('deletes task', async () => {
+            if (!user1 || !user2 || !sharedList) return;
+            expect(user1?.notifier.send).toBeCalledTimes(0);
+            const task = await createTask(sharedList._id, { title: 'just do it' }, user1);
+            // reset notification from creation
+            (user1?.notifier.send as jest.Mock).mockReset();
+            await deleteTask(task._id, user1);
+            const payload = (user1?.notifier.send as jest.Mock).mock.calls[0];
+            expect(payload[0]).toMatchObject(user2?.user._id);
+            expect(payload[1].title).toBe('unitTest deleted just do it from shared list.');
+        });
+    });
 });
