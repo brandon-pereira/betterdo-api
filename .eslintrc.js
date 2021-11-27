70740d130ecc1cbe22287b974c51fa0a02cab995
Brandon Pereira <brandonpereiras@gmail.com>
2021-11-26 17:24:22 -0700

    added eslint comments plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,15 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'plugin:eslint-comments/recommended'
     ],
     rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/no-var-requires': 0,
         'prettier/prettier': 'error',
-        'no-console': 0
+        'no-console': 0,
+        'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }]
     }
 };
