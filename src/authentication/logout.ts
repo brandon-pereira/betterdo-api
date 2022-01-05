cf9e639d4b47ab469d0155225faad2236179a450
Brandon Pereira <brandonp@criticalmass.com>
2022-01-05 14:59:31 -0700

    fix logout redirecting to app triggering login loop bug

--- src/authentication/logout.ts
+++ src/authentication/logout.ts
@@ -5,13 +5,9 @@ const serverUrl = process.env.SERVER_URL || '/';
 export default ({ app }: InternalRouter): void => {
     app.get('/auth/logout', (req, res) => {
         req.logout();
-        const referrer = req.header('referrer');
-        if (referrer) {
-            res.redirect(referrer);
-        } else {
-            res.redirect(serverUrl);
-        }
+        res.redirect(serverUrl);
     });
+
     app.post('/auth/logout', (req, res) => {
         req.logout();
         res.json({ success: true });
